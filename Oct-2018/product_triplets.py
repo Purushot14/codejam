a87202b76ef491501d6407d78e2379bc1a58166b
Purushot14 <purushothaman.kumaravel@orangescape.com>
2018-12-15 13:34:48 +0530

    product triplets with Thread

--- Oct-2018/product_triplets.py
+++ Oct-2018/product_triplets.py
@@ -3,11 +3,35 @@
     Created by prakash on 14/12/18
 """
 
+# from threading import Thread as cls
+from multiprocessing import Process as cls
+
 __author__ = 'Prakash14'
 
 
 def check_condition(nums, x, y, z):
     return nums[x] == nums[y] * nums[z] or nums[y] == nums[x] * nums[z] or nums[z] == nums[y] * nums[x]
+
+
+def _run(test_case, number_of_inputs, inputs):
+    x = 1
+    counts = 0
+    unique = set(inputs)
+    is_run = True
+    if unique.__len__() == 1 and inputs[0] != 0 and inputs[0] != 1:
+        is_run = False
+
+    while is_run and x <= number_of_inputs - 2:
+        y = x + 1
+        while y <= number_of_inputs - 1:
+            z = y + 1
+            while z <= number_of_inputs:
+                if x != y != z and check_condition(inputs, x=x - 1, y=y - 1, z=z - 1):
+                    counts += 1
+                z += 1
+            y += 1
+        x += 1
+    print(f"Case #{test_case+1}: {counts}")
 
 
 def get_product_triplets():
@@ -19,19 +43,9 @@ def get_product_triplets():
             inputs.extend([int(i) for i in input().split(" ")])
         if inputs.__len__() > number_of_inputs:
             raise ValueError("More number of inputs given")
-        x = 1
-        counts = 0
-        while x <= number_of_inputs - 2:
-            y = x + 1
-            while y <= number_of_inputs - 1:
-                z = y + 1
-                while z <= number_of_inputs:
-                    if check_condition(inputs, x=x-1, y=y-1, z=z-1):
-                        counts += 1
-                    z += 1
-                y += 1
-            x += 1
-        print(f"Case #{test_case+1}: {counts}")
 
+        cls(target=_run, kwargs={"test_case": test_case,
+                                 "number_of_inputs": number_of_inputs,
+                                 "inputs": inputs}).start()
 
 get_product_triplets()
